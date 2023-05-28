bf1298daefd84c7d62a7a7a319cc81a1df524c4e
polobryn <polobryn@yahoo.co.uk>
2023-05-28 21:56:15 +0100

    add WG logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -10,14 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-3xl font-bold underline">
+          Wolfgang Digital Lupin App
+        </h1>
       </header>
     </div>
   );
