867a8fc2c20b990f56b64c420fe83d2aebd74369
polobryn <polobryn@yahoo.co.uk>
2023-06-14 15:23:40 +0100

    remove COOP & COEP for testing

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-    app.use(function(req, res, next) {
-        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
-        res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
-        next();
-    });
-}
